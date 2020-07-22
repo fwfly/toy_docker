f5389e8eb870bd11c663d82c4cc9dfff6fb2df03
fwfly <fawafwfly@gmail.com>
2020-07-22 22:51:33 +1000

    Fix build error

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
     cmd := exec.Command("sh")
-    cmd.SysProcAttr = syscall.SysProcAttr{
+    cmd.SysProcAttr = &syscall.SysProcAttr{
         Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
     }
 
