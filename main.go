c1e7d3e95542a3c7ed0ca637464331d31b260f7d
fwfly <fawafwfly@gmail.com>
2020-08-17 23:14:15 +1000

    p17 User namespace

--- main.go
+++ main.go
@@ -11,7 +11,28 @@ func main() {
     cmd := exec.Command("bash")
     cmd.SysProcAttr = &syscall.SysProcAttr{
         Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
-        syscall.CLONE_NEWNS,
+        syscall.CLONE_NEWNS |
+        syscall.CLONE_NEWUSER,
+
+// Default gid and uid are : uid=65534(nobody) gid=65534(nogroup) groups=65534(nogroup)
+// SysProcAttr.Credential isn't supported in Ubuntu 19.10.
+// Use Mappings instead
+
+/*        UidMappings: []syscall.SysProcIDMap{
+            {
+                ContainerID: 1234,
+                HostID:      0,
+                Size:        1,
+            },
+        },
+        GidMappings: []syscall.SysProcIDMap{
+            {
+                ContainerID: 1234,
+                HostID:      0,
+                Size:        1,
+            },
+        },
+*/
     }
 
     cmd.Stdin = os.Stdin
@@ -21,5 +42,5 @@ func main() {
     if err := cmd.Run(); err != nil {
         log.Fatal(err)
     }
-
+    os.Exit(-1)
 }
