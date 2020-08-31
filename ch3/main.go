86e225201548de1fbf90581a78ec41439132be59
fwfly <fwfly@example.com>
2020-08-31 22:48:47 +1000

    Run init and run command without containers

--- ch3/main.go
+++ ch3/main.go
@@ -15,6 +15,15 @@ func main(){
     app := cli.NewApp()
     app.Name = "mydocker"
     app.Usage = usage
+    app.Commands = []*cli.Command{
+        &initCommand,
+        &runCommand,
+    }
+    app.Before = func(context *cli.Context) error {
+        log.SetFormatter(&log.JSONFormatter{})
+        log.SetOutput(os.Stdout)
+        return nil
+    }
 
     if err := app.Run(os.Args); err != nil {
         log.Fatal(err)
---
+++ ch3/main_command.go
@@ -0,0 +1,45 @@
+package main
+import (
+    "fmt"
+    log "github.com/Sirupsen/logrus"
+    "github.com/urfave/cli"
+)
+
+var runCommand = cli.Command {
+    Name: "run",
+    Usage: `Create a container with namespace and cgroups limit ie: mydocker run -ti [command]`,
+    Flags: []cli.Flag{
+        &cli.BoolFlag{
+            Name: "ti",
+            Usage: "enable tty",
+        },
+    },
+    Action: func(context *cli.Context) error {
+        if context.NArg() < 1 {
+            return fmt.Errorf("Missing container command")
+        }
+        cmd := context.Args().Get(0)
+        //tty := context.Bool("ti")
+        log.Info("Run command %s ", cmd)
+        /*
+         Run isn't implemented yet :
+         Run(tty, cmd) 
+         */
+        return nil
+    },
+}
+
+var initCommand = cli.Command{
+    Name: "init",
+    Usage: `Init container process run user's process in container. Do not call it outside`,
+    Action: func(context *cli.Context) error {
+        log.Info("init come on")
+        cmd := context.Args().Get(0)
+        log.Info("command %s ", cmd)
+        /*
+        err := container.RunContainerInitProcess(cmd, nil)
+        return err
+        */
+        return nil
+    },
+}
